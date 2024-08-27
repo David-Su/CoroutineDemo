10452cdf1cbbd7bbe1a595c553450fde6b70ebfb
SuK <543879736@qq.com>
2024-08-27 19:32:30 +0800

    commit

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.coroutinedemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        GlobalScope.launch {
+            val result1 = realSuspend()
+            val result2 = fakeSuspend()
+            Log.d("TAG", "${result1} ${result2}")
         }
+    }
+
+    private suspend fun fakeSuspend(): String {
+        return "fakeSuspend"
+    }
+
+    private suspend fun realSuspend(): String {
+        return "realSuspend"
     }
 }
