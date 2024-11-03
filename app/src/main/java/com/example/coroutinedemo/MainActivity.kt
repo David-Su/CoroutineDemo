22f1fc731999522be31e368502644fa11090975d
Bruce Wayne <543879736@qq.com>
2024-11-03 23:15:05 +0800

    commit

--- app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -2,21 +2,27 @@ package com.example.coroutinedemo
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         GlobalScope.launch {
-            val result1 = realSuspend()
-            val result2 = fakeSuspend()
-            Log.d("TAG", "${result1} ${result2}")
+            val result = listOf(
+                delaySuspend(),
+                fakeSuspend(),
+                withContextSuspend(),
+                callBackSuspend(),
+            )
+            Log.d("TAG", "$result")
         }
     }
 
@@ -24,7 +30,16 @@ class MainActivity : AppCompatActivity() {
         return "fakeSuspend"
     }
 
-    private suspend fun realSuspend(): String {
+    private suspend fun delaySuspend(): String {
+        delay(1000L)
         return "realSuspend"
+    }
+
+    private suspend fun withContextSuspend(): String = withContext(Dispatchers.Default) {
+        "withContextSuspend"
+    }
+
+    private suspend fun callBackSuspend() = suspendCancellableCoroutine<String> { cont ->
+        cont.resume("callBackSuspend")
     }
 }
